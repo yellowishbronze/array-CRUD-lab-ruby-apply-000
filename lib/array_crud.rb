54043e07a3831239bfd0faa92b4ed7504d937c2b
Bryan Teng <bteng1226@gmail.com>
2018-06-14 22:49:46 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.shift(element)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.rindex(index_number)
+  array.[index_number]
 end
 
 def retrieve_first_element_from_array(array)
